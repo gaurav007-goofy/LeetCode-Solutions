617befe91df5004fb74b8f77efb933bec66dc205
Gaurav  Saini <thegauravsaini007@gmail.com>
2022-08-30 17:37:52 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (11.62%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
-    int solve(vector<int> &dp,int n)
-    {
-        if(n==0 || n==1)
-            return 1;
-        if(dp[n]!=-1)
-            return dp[n];
+    // top-down approach (memoization)
+    
+//     int solve(vector<int> &dp,int n)
+//     {
+//         if(n==0 || n==1)
+//             return 1;
+//         if(dp[n]!=-1)
+//             return dp[n];
         
-        int ways =0;
+//         int ways =0;
         
-        for(int i=1;i<=n;i++)
+//         for(int i=1;i<=n;i++)
+//         {
+//             ways += solve(dp,i-1) * solve(dp,n-i);
+//         }
+        
+//         return dp[n]=ways;
+//     }
+    int numTrees(int num) {
+        // vector<int>dp(n+1,-1);
+        // return solve(dp,n);
+        
+        // tabulation
+        
+        vector<int>dp(num+1,1);
+        
+        for(int n=2;n<=num;n++)
         {
-            ways += solve(dp,i-1) * solve(dp,n-i);
+            int ways =0;
+            for(int i=1;i<=n;i++)
+            {
+                ways += dp[i-1] * dp[n-i];
+            }
+            dp[n] = ways;
         }
-        
-        return dp[n]=ways;
-    }
-    int numTrees(int n) {
-        vector<int>dp(n+1,-1);
-        
-        return solve(dp,n);
+        return dp[num];
     }
 };
