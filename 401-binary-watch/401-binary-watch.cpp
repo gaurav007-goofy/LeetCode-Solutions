fa783d25f067833c92cec4083da9fb1836bffacd
Gaurav  Saini <thegauravsaini007@gmail.com>
2022-08-30 15:01:15 +0530

    Time: 4 ms (37.41%), Space: 6.4 MB (62.86%) - LeetHub

--- 401-binary-watch/401-binary-watch.cpp
+++ 401-binary-watch/401-binary-watch.cpp
@@ -15,14 +15,24 @@ public:
     vector<string> readBinaryWatch(int turnedOn) {
         
         vector<string>ans;
+        vector<int>bits(60);  // for storing count of set bits from 0-59
+        for(int i=0;i<60;i++)
+        {
+            bits[i] = count_set_bits(i);
+        }
+        
         string temp;
         
         for(int i=0;i<12;i++)
         {
+            int bits_left = turnedOn - bits[i];
+            if(bits_left <0)
+                continue;
+            
             for(int j=0;j<60;j++)
             {
                 temp = "";
-                if(count_set_bits(i)+count_set_bits(j) == turnedOn)
+                if(count_set_bits(j) == bits_left)
                 {
                     string hours = to_string(i);
                     string minutes = j>9 ? to_string(j) : "0"+to_string(j);
