ef7434004463d78141b0d926173ba591fbf2e63f
Gaurav  Saini <thegauravsaini007@gmail.com>
2022-09-05 15:16:33 +0530

    Time: 4 ms (20.46%), Space: 6.2 MB (56.38%) - LeetHub

--- 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.cpp
+++ 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int countSegments(string s) {
-        stringstream ss(s);
-        
-        int cnt =0;
-        string word;
-        while(ss>>word)
-        {
-            cnt++;
-        }
-        return cnt;
+        int res = 0;
+        s.push_back(' ');
+        for(int i = 1; i < s.size(); ++i)
+          if(s[i] == ' ' && s[i-1] != ' ') ++res;
+        return res;
     }
 };
