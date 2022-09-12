c35dca7b374d4ce1fc735dc502d32b2c7febf599
Gaurav  Saini <thegauravsaini007@gmail.com>
2022-09-12 12:28:40 +0530

    Time: 232 ms (20.75%), Space: 96 MB (98.36%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -2,37 +2,26 @@ class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
         
-        int m = grid.size();
-        int n = grid[0].size();
+        //perimeter of square is 4 
+        //if 2 squares are joined perimeter becomes -> 6 why? becuase 2 edges are common and we need to subtract these 2 edges
+        //so perimeter is 4*number of squres - 2*repeating edges
         
-        int cnt =0;
-        for(int i=0;i<m;i++)
+        int count = 0; //number of squares
+        int repeat=0;
+        for(int i=0;i<grid.size();i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<grid[0].size();j++)
             {
                 if(grid[i][j]==1)
                 {
-                    if(i==0)
-                        cnt++;
-                    if(j==0)
-                        cnt++;
-                    if(i==m-1)
-                        cnt++;
-                    if(j==n-1)
-                        cnt++;
-                    if(i>0 && grid[i-1][j]==0)
-                        cnt++;
-                    if(j>0 && grid[i][j-1]==0)
-                        cnt++;
-                    if(i<m-1 && grid[i+1][j]==0)
-                        cnt++;
-                    if(j<n-1 && grid[i][j+1]==0)
-                        cnt++;
+                    count++;
+                    if(i!=0 && grid[i-1][j] == 1)repeat++;  //check for up
+                    if(j!=0 && grid[i][j-1]==1)repeat++;  //check for left
+                    //we only need to check these 2 cases as when the loop goes on all the sides will be covered for all squares -> take one example and see for yourself
                 }
             }
         }
         
-        
-        return cnt;
+        return 4*count - (2*repeat);
     }
 };
