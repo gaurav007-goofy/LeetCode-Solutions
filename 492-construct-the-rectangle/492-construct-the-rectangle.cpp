43661f23366f7fb253a3f9e4728e5ebef4c621f1
Gaurav  Saini <thegauravsaini007@gmail.com>
2022-10-01 17:58:16 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (32.99%) - LeetHub

---
+++ 492-construct-the-rectangle/492-construct-the-rectangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int width = sqrt(area);
+        
+        while(area % width)
+        {
+            width--;
+        }
+        
+        return {area/width, width};
+    }
+};
