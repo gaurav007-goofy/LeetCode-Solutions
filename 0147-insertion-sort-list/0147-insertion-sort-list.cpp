17e0fe997cb18baacb389628ed631d193a280bfd
Gaurav  Saini <thegauravsaini007@gmail.com>
2022-10-17 18:04:49 +0530

    Time: 115 ms (14.96%), Space: 9.6 MB (76.81%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.cpp
+++ 0147-insertion-sort-list/0147-insertion-sort-list.cpp
@@ -13,13 +13,11 @@ public:
     ListNode* insertionSortList(ListNode* head) {
         if(head == nullptr) return nullptr;
     
-        int temp = 0;
+        
         for(ListNode* curr = head; curr != nullptr; curr = curr->next){
             for(ListNode* prev = head; prev != curr; prev = prev->next){
                 if(curr->val < prev->val){
-                    temp = curr->val;
-                    curr->val = prev->val;
-                    prev->val = temp;
+                    swap(curr->val,prev->val);
                 }
             }
         }
